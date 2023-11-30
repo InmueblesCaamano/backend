e6235731fc7606d02429d9eccea9e6b4562adf10
manuel perez <manuelperez.0000@gmail.com>
2023-11-30 00:08:30 -0400

    qwe

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '.env' })
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const routerApi = require('./network/routerApi');
 const dbConnect = require('./dbConnection');
 const cors = require('cors')
@@ -9,6 +9,11 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 dbConnect()
+
+app.get('/',(req,res) =>{
+  res.send("Welcome")
+})
+
 routerApi(app)
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
