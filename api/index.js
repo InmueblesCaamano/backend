5b89f124b6a408c1d70ec3ecb1fce5cf8f57de3a
manuel perez <manuelperez.0000@gmail.com>
2023-11-29 22:33:18 -0400

    r

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ require('dotenv').config({ path: '.env' })
 const express = require('express');
 const app = express();
 const PORT = 5000;
-const routerApi = require('./network/routerApi');
-const dbConnect = require('./dbConnection');
+const routerApi = require('../network/routerApi');
+const dbConnect = require('../dbConnection');
 const cors = require('cors')
 
 app.use(cors())
