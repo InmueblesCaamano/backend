195235720ab91d78935c81bf261778c47f7a6ba5
manuel perez <manuelperez.0000@gmail.com>
2023-12-01 22:25:34 -0400

    images charge

--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -1,31 +1,26 @@
 const express = require('express')
-// const multer = require('multer')
 const router = express.Router()
-// const path = require('path')
 const Buildings = require('../buildings/buildingsModel')
-// const tokenGenerator = require('../../services/tokenGenerator')
-
-// const storage = multer.diskStorage({
-//     destination: './files/', // Carpeta donde se guardarán los archivos
-//     filename: (req, file, cb) => {
-//         const randomChain = tokenGenerator(5)
-//         cb(null, file.fieldname + '-' + randomChain + Date.now() + path.extname(file.originalname));
-//     }
-// })
-
-// const upload = multer({ storage })
-
-// router.post('/:id',upload.array('image',6), async (req, res) => {
 router.post('/:id', async (req, res) => {
-    // const files = req.files
-    // const _id = req.params.id
-    // const respuesta = await Buildings.findOneAndUpdate({_id},{images:files})
-    // if (respuesta){
-    //     res.json({ message: "success images", status: true, body: respuesta })
-    // }
-    // else{
-    //     res.json({ message: "No se pudo subir la imagen", status: false })
-    // }
+
+    const _id = req.params.id
+    const { url } = req.body
+
+    console.log(url)
+    try {
+        const respuesta = await Buildings.findOneAndUpdate({ _id }, { $push: { images: url } })
+        if (respuesta) {
+            res.json({ message: "success images", status: true, body: respuesta })
+        }
+        else {
+            console.log(respuesta)
+            res.json({ message: "No se pudo subir la imagen", status: false })
+        }
+        
+    } catch (error) {
+        res.json({ message: "Error de coneccion", status: false })
+        
+    }
 })
 
 
